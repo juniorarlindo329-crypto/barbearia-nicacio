72dfe313d989f05eb508b2e29ce0dee3ae051947
juniorarlindo329-crypto <juniorarlindo329@gmail.com>
2026-08-27 16:11:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', () => {});
+
+const CACHE='nicacio-v3';
+const ASSETS=['./','./index.html','./admin.html','./styles.css','./app.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
